38c882a165870d0eb7f6e2fb8b2674b9d0f68f35
Chikamnele56 <chikaobinna005@gmail.com>
2023-02-08 14:33:37 -0800

    inheritance git commit -m inheritance
    git commit -m inheritance

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
+"""A module that performs magic"""
+
+
 def is_same_class(obj, a_class):
-    """ Function that returns True/False if obj is a type of a_class
-    Args:
-    obj: object
-    a_class: class type
-    Returns:
-    True if type of obj is a_class
-    False, otherwise
-    """
-    return type(obj) is 
+    """Check if obj is an instance of a_class"""
+    if (isinstance(obj, a_class) and type(obj) is a_class):
+        return (True)
+    else:
+        return (False)

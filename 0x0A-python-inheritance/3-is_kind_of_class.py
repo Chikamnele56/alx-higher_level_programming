e425841228820fa0812ccb06ea472ddb521dd180
Chikamnele56 <chikaobinna005@gmail.com>
2023-02-08 14:49:00 -0800

    inheritance

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
+"""Trabaye Module"""
+
+
 def is_kind_of_class(obj, a_class):
-    """ Function that returns True/False if obj is an instance of a_class
-    Args:
-    obj: object
-    a_class: class type
-    Returns:
-    True if obj is an instance of a_class
-    False, otherwise
-    """
-    return isinstance(obj, a_class)
+
+    if (isinstance(obj, a_class)):
+        return (True)
+    else:
+        return (False)
--- 0x0A-python-inheritance/5-base_geometry.py
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""Later things"""
+
+
 class BaseGeometry:
-    """ Empty class """
     pass

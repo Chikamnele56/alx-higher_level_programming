9a3ba4c4c49400b69a56e084dcdeb37b81ce9857
Chikamnele56 <chikaobinna005@gmail.com>
2023-03-15 18:25:33 -0700

    task

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,6 +1,6 @@
--- Create the table force_name.
+-- Creates the table force_name.
 CREATE TABLE IF NOT EXISTS 'force_name' (
-    'id'   INT,
-    'name' VARCHAR(256) NOT NULL
+	'id'   INT,
+	'name' VARCHAR(256) NOT NULL
 );
 
